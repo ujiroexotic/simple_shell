aec5d5f7fb0d89923fb200d3871041ff88f4fdbd
ujiroexotic <jiroeruteya@gmail.com>
2022-12-22 13:06:06 -0800

    Task 16.

--- shell_loop.c
+++ shell_loop.c
@@ -4,6 +4,7 @@
  * hsh - main shell loop
  * @info: the parameter & return info struct
  * @av: the argument vector from main()
+ *
  * Return: 0 on success, 1 on error, or error code
  */
 int hsh(info_t *info, char **av)
@@ -45,10 +46,11 @@ int hsh(info_t *info, char **av)
 /**
  * find_builtin - finds a builtin command
  * @info: the parameter & return info struct
+ *
  * Return: -1 if builtin not found,
- *          0 if builtin executed successfully,
- *          1 if builtin found but not successful,
- *          -2 if builtin signals exit()
+ *			0 if builtin executed successfully,
+ *			1 if builtin found but not successful,
+ *			-2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
@@ -78,6 +80,7 @@ int find_builtin(info_t *info)
 /**
  * find_cmd - finds a command in PATH
  * @info: the parameter & return info struct
+ *
  * Return: void
  */
 void find_cmd(info_t *info)
@@ -106,7 +109,7 @@ void find_cmd(info_t *info)
 	else
 	{
 		if ((interactive(info) || _getenv(info, "PATH=")
-					|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
 			fork_cmd(info);
 		else if (*(info->arg) != '\n')
 		{
@@ -119,6 +122,7 @@ void find_cmd(info_t *info)
 /**
  * fork_cmd - forks a an exec thread to run cmd
  * @info: the parameter & return info struct
+ *
  * Return: void
  */
 void fork_cmd(info_t *info)
