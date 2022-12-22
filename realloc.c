67d7f7443d1cdb24746480903c575f70eea7b62a
ujiroexotic <jiroeruteya@gmail.com>
2022-12-22 13:03:49 -0800

    Task 14.

--- realloc.c
+++ realloc.c
@@ -2,10 +2,10 @@
 
 /**
  **_memset - fills memory with a constant byte
- * @s: the pointer to the memory area
- * @b: the byte to fill *s with
- * @n: the amount of bytes to be filled
- * Return: (s) a pointer to the memory area s
+ *@s: the pointer to the memory area
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be filled
+ *Return: (s) a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -31,12 +31,12 @@ void ffree(char **pp)
 	free(a);
 }
 
-
 /**
  * _realloc - reallocates a block of memory
  * @ptr: pointer to previous malloc'ated block
  * @old_size: byte size of previous block
  * @new_size: byte size of new block
+ *
  * Return: pointer to da ol'block nameen.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
