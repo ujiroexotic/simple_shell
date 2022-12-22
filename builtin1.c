23e66bb6306f36f75de766e5fb0de80e757d5fc6
ujiroexotic <jiroeruteya@gmail.com>
2022-12-22 12:41:23 -0800

    Task 1.

--- builtin1.c
+++ builtin1.c
@@ -5,7 +5,7 @@
  *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
- * Return: Always 0
+ *  Return: Always 0
  */
 int _myhistory(info_t *info)
 {
@@ -31,16 +31,16 @@ int unset_alias(info_t *info, char *str)
 	c = *p;
 	*p = 0;
 	ret = delete_node_at_index(&(info->alias),
-			get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
 	*p = c;
 	return (ret);
 }
-
 
 /**
  * set_alias - sets alias to string
  * @info: parameter struct
  * @str: the string alias
+ *
  * Return: Always 0 on success, 1 on error
  */
 int set_alias(info_t *info, char *str)
@@ -60,6 +60,7 @@ int set_alias(info_t *info, char *str)
 /**
  * print_alias - prints an alias string
  * @node: the alias node
+ *
  * Return: Always 0 on success, 1 on error
  */
 int print_alias(list_t *node)
@@ -83,7 +84,7 @@ int print_alias(list_t *node)
  * _myalias - mimics the alias builtin (man alias)
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- * Return: Always 0
+ *  Return: Always 0
  */
 int _myalias(info_t *info)
 {
@@ -109,8 +110,6 @@ int _myalias(info_t *info)
 		else
 			print_alias(node_starts_with(info->alias, info->argv[i], '='));
 	}
+
 	return (0);
 }
-
-
-
