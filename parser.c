ff1b2a351fda6276ee25fadad59dfd830e01db94
ujiroexotic <jiroeruteya@gmail.com>
2022-12-22 12:57:34 -0800

    Task 1.

--- parser.c
+++ parser.c
@@ -4,6 +4,7 @@
  * is_cmd - determines if a file is an executable command
  * @info: the info struct
  * @path: path to the file
+ *
  * Return: 1 if true, 0 otherwise
  */
 int is_cmd(info_t *info, char *path)
@@ -26,6 +27,7 @@ int is_cmd(info_t *info, char *path)
  * @pathstr: the PATH string
  * @start: starting index
  * @stop: stopping index
+ *
  * Return: pointer to new buffer
  */
 char *dup_chars(char *pathstr, int start, int stop)
@@ -45,6 +47,7 @@ char *dup_chars(char *pathstr, int start, int stop)
  * @info: the info struct
  * @pathstr: the PATH string
  * @cmd: the cmd to find
+ *
  * Return: full path of cmd if found or NULL
  */
 char *find_path(info_t *info, char *pathstr, char *cmd)
