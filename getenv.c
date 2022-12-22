477812d8f00138d31af1ce2aa42baa3cb12690b1
ujiroexotic <jiroeruteya@gmail.com>
2022-12-22 12:44:59 -0800

    Task 5.

--- getenv.c
+++ getenv.c
@@ -21,7 +21,7 @@ char **get_environ(info_t *info)
  * _unsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
- * Return: 1 on delete, 0 otherwise
+ *  Return: 1 on delete, 0 otherwise
  * @var: the string env var property
  */
 int _unsetenv(info_t *info, char *var)
@@ -51,12 +51,12 @@ int _unsetenv(info_t *info, char *var)
 
 /**
  * _setenv - Initialize a new environment variable,
- *              or modify an existing one
+ *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  * @var: the string env var property
  * @value: the string env var value
- * Return: Always 0
+ *  Return: Always 0
  */
 int _setenv(info_t *info, char *var, char *value)
 {
