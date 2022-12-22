2c75fcbe95b0326fdf05254321cfcb20c62eca1e
ujiroexotic <jiroeruteya@gmail.com>
2022-12-22 12:50:02 -0800

    Task 10.

--- lists.c
+++ lists.c
@@ -5,6 +5,7 @@
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
+ *
  * Return: size of list
  */
 list_t *add_node(list_t **head, const char *str, int num)
@@ -32,13 +33,12 @@ list_t *add_node(list_t **head, const char *str, int num)
 	return (new_head);
 }
 
-
-
 /**
  * add_node_end - adds a node to the end of the list
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
+ *
  * Return: size of list
  */
 list_t *add_node_end(list_t **head, const char *str, int num)
@@ -77,6 +77,7 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 /**
  * print_list_str - prints only the str element of a list_t linked list
  * @h: pointer to first node
+ *
  * Return: size of list
  */
 size_t print_list_str(const list_t *h)
@@ -97,6 +98,7 @@ size_t print_list_str(const list_t *h)
  * delete_node_at_index - deletes node at given index
  * @head: address of pointer to first node
  * @index: index of node to delete
+ *
  * Return: 1 on success, 0 on failure
  */
 int delete_node_at_index(list_t **head, unsigned int index)
@@ -135,6 +137,7 @@ int delete_node_at_index(list_t **head, unsigned int index)
 /**
  * free_list - frees all nodes of a list
  * @head_ptr: address of pointer to head node
+ *
  * Return: void
  */
 void free_list(list_t **head_ptr)
