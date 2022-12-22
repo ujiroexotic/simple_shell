cb8969010c8f9cb753349637e086a177c53c5822
ujiroexotic <jiroeruteya@gmail.com>
2022-12-22 13:08:10 -0800

    Task 19

--- vars.c
+++ vars.c
@@ -5,6 +5,7 @@
  * @info: the parameter struct
  * @buf: the char buffer
  * @p: address of current position in buf
+ *
  * Return: 1 if chain delimeter, 0 otherwise
  */
 int is_chain(info_t *info, char *buf, size_t *p)
@@ -41,6 +42,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
  * @p: address of current position in buf
  * @i: starting position in buf
  * @len: length of buf
+ *
  * Return: Void
  */
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
@@ -67,10 +69,10 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 	*p = j;
 }
 
-
 /**
  * replace_alias - replaces an aliases in the tokenized string
  * @info: the parameter struct
+ *
  * Return: 1 if replaced, 0 otherwise
  */
 int replace_alias(info_t *info)
@@ -99,6 +101,7 @@ int replace_alias(info_t *info)
 /**
  * replace_vars - replaces vars in the tokenized string
  * @info: the parameter struct
+ *
  * Return: 1 if replaced, 0 otherwise
  */
 int replace_vars(info_t *info)
@@ -114,23 +117,24 @@ int replace_vars(info_t *info)
 		if (!_strcmp(info->argv[i], "$?"))
 		{
 			replace_string(&(info->argv[i]),
-					_strdup(convert_number(info->status, 10, 0)));
+				_strdup(convert_number(info->status, 10, 0)));
 			continue;
 		}
 		if (!_strcmp(info->argv[i], "$$"))
 		{
 			replace_string(&(info->argv[i]),
-					_strdup(convert_number(getpid(), 10, 0)));
+				_strdup(convert_number(getpid(), 10, 0)));
 			continue;
 		}
 		node = node_starts_with(info->env, &info->argv[i][1], '=');
 		if (node)
 		{
 			replace_string(&(info->argv[i]),
-					_strdup(_strchr(node->str, '=') + 1));
+				_strdup(_strchr(node->str, '=') + 1));
 			continue;
 		}
 		replace_string(&info->argv[i], _strdup(""));
+
 	}
 	return (0);
 }
@@ -139,6 +143,7 @@ int replace_vars(info_t *info)
  * replace_string - replaces string
  * @old: address of old string
  * @new: new string
+ *
  * Return: 1 if replaced, 0 otherwise
  */
 int replace_string(char **old, char *new)
